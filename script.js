5949c59d09c026c4f4b195ffae40b0ff9844b3f6
Game Maker <br3ndan.l8n@gmail.com>
2020-07-08 16:11:44 -0400

    Got the colors to change randomly.

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let card = document.querySelector('#card');
 let cardTitle = document.querySelector('#cardTitle');
 
 function imagePressed() {
-
+    assignColorsToElemets();
 }
 
 function randomizeRGB(colorVariable) {
@@ -24,11 +24,36 @@ function randomizeRGB(colorVariable) {
 }
 
 function assignColorsToElemets() {
-    body.style.backgroundColor = backgroundPrimary;
-    title.style.backgroundColor = backgroundPrimary;
-    bodyHeader.style.backgroundColor = backgroundTertiary1;
+    // Placeholder Variable For Use in randomizeRGB()
+    let tempColorVar = "";
+    // Assigning Random Colors To BACKGROUND Color Variables
+    const backgroundPrimary = randomizeRGB(tempColorVar);
+    const backgroundTertiary1 = randomizeRGB(tempColorVar);
+    const backgroundTertiary2 = randomizeRGB(tempColorVar);
+    // Assigning Random Colors To FONT Color Variables
+    const textTitle = randomizeRGB(tempColorVar);
+    const textPrimary = randomizeRGB(tempColorVar);
 
+    // Assigning Color Variables to Document Objects
+    // BACKGROUND primary
+    body.style.backgroundColor = backgroundPrimary;
+    // BACKGROUND tertiary
+    title.style.backgroundColor = backgroundTertiary1;
+    cardTitle.style.backgroundColor = backgroundTertiary1;
+    card.style.backgroundColor = backgroundTertiary2;
+    // FONT primary
     body.style.color = textPrimary;
+    // FONT tertiary
+    title.style.color = textTitle;
+    cardTitle.style.color = textTitle;
+    
+
+    console.log(`==============================================`)
+    console.log(`Primary Background Color: ${backgroundPrimary}`)
+    console.log(`Tertiary 1 Background Color: ${backgroundTertiary1}`)
+    console.log(`Tertiary 2 Background Color: ${backgroundTertiary2}`)
+
+    console.log(`Primary Font Color: ${textPrimary}`)
 
 }
 
